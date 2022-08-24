8861dc24688393ca831d168fcc84cbe6778f258b
Евгений Мамадалиев <evgenijmamadaliev@MacBook-Pro-Evgenij.local>
2022-08-24 11:28:10 +0300

    new

---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿
+//  Урок 1. Знакомство с языками програмирования 
+//    Задача 2
+/*
+Console.WriteLine("start programm, Please input numberA");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input numberB");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if ( number1 > number2 )
+{   
+    Console.WriteLine($"{number1} bigges");
+}
+else 
+{ 
+    Console.WriteLine($"{number2} bigges");
+}      
+*/
+
+
+
+//  Задача 4 
+
+//Напишите программу которая принимает на вход три числа и выдает максимальное 
+//из этих чисел
+
+/*
+Console.WriteLine("start programm, Please input number1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input number2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input number3");
+int number3 = Convert.ToInt32(Console.ReadLine());
+ 
+
+int max = 0;
+
+if ( number1 > max )
+
+{   
+    max = number1;
+}
+if (number2 > max)
+{ max = number2;
+}
+if (number3 > max)
+{ max = number3;
+}
+Console.WriteLine ($"{max} is bigges");
+
+*/
+
+// задача 6
+// Напишите программу которая на вход принимает число и выдает, является
+//ли это число четным или нечетным( делится ли оно без остатка )
+
+/*
+Console.WriteLine("start programm, Please input number");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if (number1 % 2 == 0)  //проверка на четность 
+Console.WriteLine ($"{number1} четное число ");
+
+else 
+Console.WriteLine ($"{number1} НЕчетное число ");
+  
+*/
+
+
+// Задача 8
+// напишите программу которая на вход принимает число (N)
+// а на выходе показывает все четные числа от 1 до N
+
+/*
+Console.WriteLine("start programm, Please input N");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+current = 1;
+
+while i < N
+{
+
+    if current (a % 2 == 0)
+
+
+{
+
+
+Console.Write (Concurrent + " ");
+}
+i++;
+Current++;
+
+}
+
+*/
